1f99993660851cc4907832171801363dc21915f9
Zisheng Ye <zisheng_ye@outlook.com>
2019-12-05 14:48:27 -0600

    increase the number of particles of the surface of rigid body

--- src/stokes.cpp
+++ src/stokes.cpp
@@ -77,8 +77,8 @@ void GMLS_Solver::StokesEquation() {
 
   double epsilonMultiplier = 1.6;
   int estimatedUpperBoundNumberNeighbors =
-      pointCloudSearch.getEstimatedNumberNeighborsUpperBound(
-          minNeighbors, __dim, epsilonMultiplier);
+      8 * pointCloudSearch.getEstimatedNumberNeighborsUpperBound(
+              minNeighbors, __dim, epsilonMultiplier);
 
   Kokkos::View<int **, Kokkos::DefaultExecutionSpace> neighborListsDevice(
       "neighbor lists", numTargetCoords, estimatedUpperBoundNumberNeighbors);
