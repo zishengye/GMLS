52257faa4a8f8365532cb6622f219249333f744c
Zisheng Ye <zisheng_ye@outlook.com>
2020-01-10 14:49:01 -0600

    add refinement

--- src/make_geometry_rigid_body.cpp
+++ src/make_geometry_rigid_body.cpp
@@ -14,7 +14,7 @@ void GMLS_Solver::InitRigidBody() {
       __rigidBody.vector.Register("angular velocity");
   vector<double> &rigidBodySize = __rigidBody.scalar.Register("size");
   // initialize data storage
-  int Nr = 8;
+  int Nr = 0;
 
   rigidBodyPosition.resize(Nr);
   rigidBodyOrientation.resize(Nr);
@@ -27,45 +27,15 @@ void GMLS_Solver::InitRigidBody() {
   // rigidBodyPosition[0][2] = 0.0;
   // rigidBodySize[0] = 0.1;
 
-  rigidBodyPosition[0][0] = -1.0;
-  rigidBodyPosition[0][1] = 0.0;
-  rigidBodyPosition[0][2] = 1.0;
-  rigidBodySize[0] = 1.0;
+  // rigidBodyPosition[0][0] = -1.0;
+  // rigidBodyPosition[0][1] = 0.0;
+  // rigidBodyPosition[0][2] = 1.0;
+  // rigidBodySize[0] = 1.0;
 
-  rigidBodyPosition[1][0] = 0.0;
-  rigidBodyPosition[1][1] = 0.0;
-  rigidBodyPosition[1][2] = -1.5;
-  rigidBodySize[1] = 1.0;
-
-  rigidBodyPosition[2][0] = 1.1;
-  rigidBodyPosition[2][1] = 2.3;
-  rigidBodyPosition[2][2] = 2.1;
-  rigidBodySize[2] = 1.0;
-
-  rigidBodyPosition[3][0] = -3.0;
-  rigidBodyPosition[3][1] = 4.0;
-  rigidBodyPosition[3][2] = 3.7;
-  rigidBodySize[3] = 1.0;
-
-  rigidBodyPosition[4][0] = -3.1;
-  rigidBodyPosition[4][1] = -3.0;
-  rigidBodyPosition[4][2] = 4.0;
-  rigidBodySize[4] = 1.0;
-
-  rigidBodyPosition[5][0] = 2.0;
-  rigidBodyPosition[5][1] = -4.3;
-  rigidBodyPosition[5][2] = 2.0;
-  rigidBodySize[5] = 1.0;
-
-  rigidBodyPosition[6][0] = 4.0;
-  rigidBodyPosition[6][1] = -3.5;
-  rigidBodyPosition[6][2] = -2.0;
-  rigidBodySize[6] = 1.0;
-
-  rigidBodyPosition[7][0] = -0.5;
-  rigidBodyPosition[7][1] = 2.5;
-  rigidBodyPosition[7][2] = -3.0;
-  rigidBodySize[7] = 1.0;
+  // rigidBodyPosition[1][0] = 0.0;
+  // rigidBodyPosition[1][1] = 0.0;
+  // rigidBodyPosition[1][2] = -1.5;
+  // rigidBodySize[1] = 1.0;
 
   // rigidBodyCoord[0][0] = 0;
   // rigidBodyCoord[0][1] = 0;
