185ed751a871287acab42836e4cd7aea413d6ace
Zisheng Ye <zisheng_ye@outlook.com>
2020-02-25 10:22:51 -0600

    improve refinement strategy

--- src/make_geometry.cpp
+++ src/make_geometry.cpp
@@ -728,8 +728,13 @@ void GMLS_Solver::SplitParticle(vector<int> &splitTag) {
 
   vector<int> gapParticleSplitTag(numTargetCoords);
   for (int i = 0; i < numTargetCoords; i++) {
-    if (gapParticleSize[i][0] >
-        backgroundParticleSize[backgroundSourceIndex[neighborLists(i, 1)]][0]) {
+    if ((backgroundSplitTag[backgroundSourceIndex[neighborLists(i, 1)]] == 1 &&
+         gapParticleSize[i][0] >
+             0.5 * backgroundParticleSize[backgroundSourceIndex[neighborLists(
+                       i, 1)]][0]) ||
+        gapParticleSize[i][0] >
+            backgroundParticleSize[backgroundSourceIndex[neighborLists(i, 1)]]
+                                  [0]) {
       gapParticleSplitTag[i] = 1;
     } else {
       gapParticleSplitTag[i] = 0;
