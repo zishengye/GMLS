e26b2994ea32ccc8bff9ccd2672ff3dc471b1dc3
Zisheng Ye <zisheng_ye@outlook.com>
2021-10-31 18:50:52 -0500

    fix volume on boundary

--- src/refinement.cpp
+++ src/refinement.cpp
@@ -36,7 +36,7 @@ bool gmls_solver::refinement() {
   auto &local_spacing = *(geo_mgr->get_current_work_particle_spacing());
 
   // mark stage
-  double alpha = 0.9;
+  double alpha = 0.99;
 
   PetscPrintf(PETSC_COMM_WORLD, "alpha: %f\n", alpha);
 
